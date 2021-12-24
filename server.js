180c54e7ba6b34a58e53b805274d68747888c684
rudnfcks <rudnfcks@naver.com>
2021-12-24 23:00:11 +0900

    Node & React Peristalsis

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const http = require("http").createServer(app);
 const port = 5000;
 
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+// Server Listen
 http.listen(port, () => {
   console.log(`Server is Running on ${port}`);
+});
+
+// Static Setting
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(
+  "/tic_tac_toe",
+  express.static(path.join(__dirname, "web_tic_tac_toe/build"))
+);
+
+// Main Page
+app.get("/", function (request, response) {
+  response.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Tic Tac Toe Game
+app.get("/tic_tac_toe", function (request, response) {
+  response.sendFile(path.join(__dirname, "/web_tic_tac_toe/build/index.html"));
+});
+app.get("/tic_tac_toe/*", function (request, response) {
+  response.redirect("/tic_tac_toe");
 });
